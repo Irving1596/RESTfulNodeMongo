7a86e896886956c68905975166eb7adb8292d4b1
“Irving <Irving>
2020-10-24 23:13:53 -0500

    restserver

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(require('./routes/usuario'));
+
+
+async function startbd() {
+    try {
+        console.log('Initializing database module');
+        await mongoose.connect(process.env.URLDB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+    } catch (err) {
+        console.error(err);
+
+        process.exit(1); // Non-zero failure code
+    }
+}
+startbd();
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
