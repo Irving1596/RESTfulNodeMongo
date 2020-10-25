efe05a37db0906542282165721f23c774f6e9bc0
“Irving <Irving>
2020-10-25 00:10:32 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admincafe:lCsivGdeFC41qzR4@cafe.snnhx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
